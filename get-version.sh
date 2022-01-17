75404b768bd7194f1435f0be3d5d46fb5d62290f
Frantisek Zacek <frantisek.zacek@imgarena.com>
2022-01-17 15:35:02 +0000

    Change Maven strategy

--- get-version.sh
+++ get-version.sh
@@ -3,7 +3,7 @@
 if [ -f ./pom.xml ] ; then
     DEPENDENCY_SYSTEM=MAVEN
     BUILD_FILE=./pom.xml
-    grep -m 1 "^    <version>.*</version>" ${BUILD_FILE} | awk -F'[><]' '{print $3}' | tr -d '\n'
+    mvn help:evaluate -Dexpression=project.version -q -DforceStdout
 else
     DEPENDENCY_SYSTEM=GRADLE
     if [ -f ./build.gradle ] ; then
