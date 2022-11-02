8d127742527838913bfd7c75ab15f1a2d4b4e094
Oliver Freeman <oliver.freeman@imgarena.com>
2022-11-02 16:10:47 +0000

    Update the action to improve the functionality
    * Update the README with all the necessary info
    * Clean up unnecessary code calls
    * Use the env variables to define the author of the commit, which is the actor and the committer which is usually a "bot"
    * Handle the issue of protected branches by removing the "checkout" action header, using the provided token to push and then reapplying the header
    * Use the commit message to prevent the push from triggering the workflow to run.

--- get-version.sh
+++ get-version.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
 if [ -f ./pom.xml ] ; then
-    DEPENDENCY_SYSTEM=MAVEN
-    BUILD_FILE=./pom.xml
     mvn help:evaluate -Dexpression=project.version -q -DforceStdout
 else
-    DEPENDENCY_SYSTEM=GRADLE
-    if [ -f ./build.gradle ] ; then
-        BUILD_FILE=./build.gradle
-    elif [ -f ./build.gradle.kts ]; then
-        BUILD_FILE=./build.gradle.kts
-    fi
-    ./gradlew properties --no-daemon --console=plain -q --build-file "$BUILD_FILE" | grep "^version:" | awk '{printf $2}'
+    ./gradlew properties --no-daemon --console=plain -q | grep "^version:" | awk '{printf $2}'
 fi
